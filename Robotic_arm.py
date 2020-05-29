9a4505ec8425f5f5fac9b9e841a049883a11936b
dave-roo <35938373+dave-roo@users.noreply.github.com>
2020-05-29 06:54:02 +0200

    simplify the changes, removed larger matrix, fixed issues

--- Robotic_arm.py
+++ Robotic_arm.py
@@ -23,7 +23,7 @@ TCPc = 0
 
 def Joint1(WPx, WPy):
     J1 = math.atan2(WPy, WPx)
-    print("J1=" + str(J1))
+    print("J1=" + str(round(J1,2)))
   
 def Joint2(alpha, B):
     J2 = round((math.pi/2),2) - alpha - B #B is positive if in down state
@@ -45,38 +45,43 @@ def Joint6(R12, R13):
     J6 = math.atan2(R12, R13)
     print("J6=" + str(round(J6,2)))
 
-Rx = np.array([[1, 0, 0], [0, math.cos(TCPa), -math.sin(TCPa)], [0, math.sin(TCPa), math.cos(TCPa)]])
-Ry = np.array([[math.cos(TCPb), 0, math.sin(TCPb)], [0, 1, 0], [-math.sin(TCPb), 0, math.cos(TCPb)]])
-Rz = np.array([[math.cos(TCPc), -math.sin(TCPc), 0], [math.sin(TCPc), math.cos(TCPc), 0], [0, 0, 1]])
+#Rx = np.array([[1, 0, 0], [0, math.cos(TCPa), -math.sin(TCPa)], [0, math.sin(TCPa), math.cos(TCPa)]])
+#Ry = np.array([[math.cos(TCPb), 0, math.sin(TCPb)], [0, 1, 0], [-math.sin(TCPb), 0, math.cos(TCPb)]])
+#Rz = np.array([[math.cos(TCPc), -math.sin(TCPc), 0], [math.sin(TCPc), math.cos(TCPc), 0], [0, 0, 1]])
 
 #R = Rx(C)Ry(B)Rz(A)
+#RXY = Rx.dot(Ry)
+#RXYZ = RXY.dot(Rz)
 
-RXY = Rx.dot(Ry)
+# x=A; y =B; z=C)
+A = TCPa;
+B = TCPb;
+C = TCPc;
 
-RXYZ = RXY.dot(Rz)
-print("Rxyz" + str(RXYZ))
+R = np.array([[math.cos(C)*math.cos(B), math.cos(C)*math.sin(B)*math.sin(A)-math.sin(C)*math.cos(A), math.sin(C)*math.sin(A)+math.cos(C)*math.cos(B)*math.cos(A)],
+              [math.sin(C)*math.cos(B), math.cos(C)*math.cos(A)+math.sin(C)*math.sin(B)*math.sin(A), math.sin(C)*math.sin(B)*math.cos(A)-math.cos(C)*math.sin(A)],
+              [-math.sin(B), math.cos(B)*math.sin(A), math.cos(B)*math.cos(A)]])
 
-x = np.array([[1],[0],[0]])
+print(R)
 
-x_hat = RXYZ.dot(x)
+#print(RXYZ)
 
-R12= RXYZ[0][1]
-R13= RXYZ[0][2]
+R11= R[0][0]
+R21= R[1][0]
+R31= R[2][0]
 
-R11 = x_hat[0]
-R21 = x_hat[1]
-R31 = x_hat[2]
-
-
+print(R11)
+print(R21)
+print(R31)
 
 WPx = TCPx - a6x * R11
-print("WPx " + str(WPx[0]))
+print("WPx " + str(WPx))
 
 WPy = TCPy - a6x * R21
-print("WPy " + str(WPy[0]))
+print("WPy " + str(WPy))
 
 WPz = TCPz - a6x * R31
-print("WPz " + str(WPz[0]))
+print("WPz " + str(WPz))
 
 WPxy = math.sqrt(math.pow(WPx,2) + math.pow(WPy,2))
 
@@ -105,6 +110,6 @@ delta = math.atan2(a4x + a5x, a4z)
 Joint1(WPx, WPy)
 Joint2(alpha, B)
 Joint3(delta, y)
-Joint4(R21, R31)
-Joint5(R11)
-Joint6(R12, R13)
+#Joint4(R21, R31)
+#Joint5(R11)
+#Joint6(R12, R13)
